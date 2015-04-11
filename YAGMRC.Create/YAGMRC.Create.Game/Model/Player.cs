40f87513f6d0fbd90d7250a3a0b5fc97279bf0cb
Alexander Bleissem <alexander@bleissem.com>
2015-04-11 22:06:28 +0200

    added some stubs

---
+++ YAGMRC.Create/YAGMRC.Create.Game/Model/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAGMRC.Create.Game.Model
+{
+    public class Player
+    {
+
+        #region constructor
+
+        private Player()
+        {
+
+        }
+
+        public Player(string name, string email)
+        {
+            this.Name = name;
+            this.EMail = email;
+            this.ID = Guid.NewGuid();
+        }
+
+        #endregion
+
+        public string Name { get; set; }
+
+        public string EMail { get; set; }
+
+        public Guid ID { get; set; }
+
+
+    }
+}
---
+++ YAGMRC.Create/YAGMRC.Create.Google/CreateGameAtGoogle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAGMRC.Create.Google
+{
+    public class CreateGameAtGoogle
+    {
+        #region constructor
+
+        public CreateGameAtGoogle()
+        {
+
+        }
+
+        #endregion
+
+    }
+}
