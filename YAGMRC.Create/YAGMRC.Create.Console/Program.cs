e114292c8bd9c39d2fe01bd22a39c92820ef3b7a
Alexander Bleissem <alexander@bleissem.com>
2015-04-11 15:17:22 +0200

    #2 - add google service

---
+++ YAGMRC.Create/YAGMRC.Create.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAGMRC.Create.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your google name:");
+            string name = Console.ReadLine();
+
+            var service = YAGMRC.Create.Google.GoogleService.GetInstance().Service(name);
+
+        }
+    }
+}
